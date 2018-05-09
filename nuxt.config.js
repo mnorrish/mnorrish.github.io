1ada802281f28324103d492b36c278e35a97f0dd
Matthew Norrish <matt.norrish@clavisinsight.com>
2018-05-10 11:37:04 +1200

    add head links and scripts

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,22 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "msvalidate.01", content: "4A176D1DBACD7F686445A6E2F3269816" },
     ],
+    link: [{ rel: "canonical", href: "http://mnorrish.co.nz" }],
+    script: [
+      {
+        type: "text/javascript",
+        src: "https://www.googletagmanager.com/gtag/js?id=UA-33183970-1",
+        async: true,
+      },
+      {
+        type: "text/javascript",
+        innerHTML:
+          "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-33183970-1');",
+      },
+    ],
+    __dangerouslyDisableSanitizers: ["script"],
   },
   /*
   ** Customize the progress bar color
