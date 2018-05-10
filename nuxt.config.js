66fe2bd3d855c713831224241f0950774d37fd34
Matthew Norrish <matt.norrish@clavisinsight.com>
2018-05-10 16:52:28 +1200

    don't lint during build

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,19 +31,5 @@ module.exports = {
   /*
   ** Build configuration
   */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend(config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: "pre",
-          test: /\.(js|vue)$/,
-          loader: "eslint-loader",
-          exclude: /(node_modules)/,
-        });
-      }
-    },
-  },
+  build: {},
 };
